809c5e0e8f52d2ee261723e2305bb94cab93a34f
krish-ds <krishds@users.noreply.github.com>
2020-07-06 13:06:32 +0530

    commit in 'master'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Instabot", # Replace with your own username
+    name="Insta_bot", # Replace with your own username
     version="0.0.1",
     author="Gokula Krishna",
     author_email="trgokul99@gmail.com",
